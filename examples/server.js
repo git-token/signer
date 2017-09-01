cc96d771184ea7a130bb4e004820430438e338f0
Ryan Tate <ryan.michael.tate@gmail.com>
2017-08-31 17:10:06 -0700

    fixing syntax error

--- examples/server.js
+++ examples/server.js
@@ -2,7 +2,7 @@ const GitTokenSigner = require('../dist/index').default
 const { signerIpcPath, recover } = require('../config')
 
 let signer = new GitTokenSigner({
-  recover
+  recover,
   signerIpcPath,
   web3Provider: 'http://138.68.225.133:8545',
   dirPath: process.cwd(),
