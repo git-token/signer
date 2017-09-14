8252e7ec8eba33581d2cb8ae8dbb746ced2ce510
Ryan Tate <ryan.michael.tate@gmail.com>
2017-09-14 15:48:40 -0700

    WIP

--- dist/server/sign/index.js
+++ dist/server/sign/index.js
@@ -1,1 +1,19 @@
-"use strict";
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.transaction = exports.deploy = undefined;
+
+var _deploy = require('./deploy');
+
+var _deploy2 = _interopRequireDefault(_deploy);
+
+var _transaction = require('./transaction');
+
+var _transaction2 = _interopRequireDefault(_transaction);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.deploy = _deploy2.default;
+exports.transaction = _transaction2.default;
---
+++ src/server/sign/index.js
@@ -0,0 +1,7 @@
+import deploy from './deploy'
+import transaction from './transaction'
+
+export {
+  deploy,
+  transaction
+}
